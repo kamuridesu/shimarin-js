d2691624f29892ba8d70109fc1a393480b7607e6
Kamuri Amorim <myk.gata14@gmail.com>
2024-07-26 19:00:33 -0300

    build: add build script

---
+++ publish.sh
@@ -0,0 +1,8 @@
+rm -rf dist;
+npm run build || { echo 'Failed to build package!' ; exit 1; }
+sed "s/dist\/cli.js/cli.js/;s/files/a/;s/dist//;s/--outDir/--outDir dist/;s/cd  /cd dist/;s/tsc/echo/" package.json > dist/package.json;
+cp README.md dist;
+cd dist;
+npm pkg fix || { echo 'Failed to build package!' ; exit 1; }
+npm publish || { echo 'Failed to publish package!' ; exit 1; }
+cd -;
